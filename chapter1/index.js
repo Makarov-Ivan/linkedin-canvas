4bdec0c74c49b448da64d1bb0b29c8ebe83c3302
Ivan Makarov <makivanik@gmail.com>
2022-04-06 00:14:41 +0200

    [style] add semicolons in js code

--- chapter1/index.js
+++ chapter1/index.js
@@ -3,9 +3,10 @@ window.addEventListener('load', () => {
     canvas.width = 350;
     canvas.height = 350;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'lightBlue'
-    ctx.strokeStyle = 'blue'
+    ctx.fillStyle = 'lightBlue';
+    ctx.strokeStyle = 'blue';
     ctx.lineWith = 5;
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
 })
