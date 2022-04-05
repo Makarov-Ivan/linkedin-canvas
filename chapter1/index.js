c955fac01e555cba34c1fdfc9c4bd23c195216f3
Ivan Makarov <makivanik@gmail.com>
2022-04-06 00:09:07 +0200

    draw blue rect in canvas

---
+++ chapter1/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1');
+    canvas.width = 350;
+    canvas.height = 350;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'lightBlue'
+    ctx.strokeStyle = 'blue'
+    ctx.lineWith = 5;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+})
