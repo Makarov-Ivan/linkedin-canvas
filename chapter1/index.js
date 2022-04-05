0b57bbca1e68be0a1742ac3489c2e27d3a198910
Ivan Makarov <makivanik@gmail.com>
2022-04-06 00:36:14 +0200

    render squere just in the middle of canvas

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas1');
-    canvas.width = 350;
-    canvas.height = 350;
+    canvas.width = 150;
+    canvas.height = 950;
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'lightBlue';
     ctx.strokeStyle = 'blue';
@@ -9,4 +9,22 @@ window.addEventListener('load', () => {
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+    const getSmallEdge = context => {
+        const edges = [context.canvas.height, context.canvas.width];
+        edges.sort((a, b) => a - b);
+        return edges[0]
+    }
+
+    const getSquareParams = (context, baseValue) => {
+        const smallEdge = getSmallEdge(context);
+        const edge = smallEdge * baseValue;
+        return {
+            x: (context.canvas.width - edge) / 2,
+            y: (context.canvas.height - edge) / 2,
+            width: edge,
+            height: edge,
+        };
+    };
+    const squareParams = getSquareParams(ctx, 1);
+    ctx.clearRect(squareParams.x, squareParams.y, squareParams.width, squareParams.height);
 })
